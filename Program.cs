464615697e4f8d9286a075bbd629322c8d8dafc6
JustinnDev <120927377+JustinnDev@users.noreply.github.com>
2023-03-11 15:45:53 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Colcar cuantas veces se repitio cada una de las palabras de un texto
+
+List<string> list = Console.ReadLine().ToLower().Split(" ").ToList();
+
+list.Sort();
+
+int count = 1;
+
+for (int i = 0; i < list.Count - 1; i++)
+{
+    if (list[i] == list[i + 1]) count++;
+
+    else
+    {
+        Console.WriteLine($"{list[i]} = {count}");
+        count = 1;
+    }
+}
+
